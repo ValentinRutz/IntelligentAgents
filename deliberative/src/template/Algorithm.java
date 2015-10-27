f55f2348261e36fdf85403605cc76b153c55efa7
Valentin Rutz <valentin.rutz@epfl.ch>
2015-10-27 10:13:19 +0100

    Get rid of weird import that crashes the whole project

--- deliberative/src/template/Algorithm.java
+++ deliberative/src/template/Algorithm.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +11,6 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 
-import epfl.lia.logist.core.topology.Topology;
 import logist.plan.Action;
 import logist.plan.Plan;
 import logist.simulation.Vehicle;
